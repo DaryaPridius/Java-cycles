e67c6f2e79d1c77162e92c32277b96f1af211165
Pridius <darya.pridius@yandex.ru>
2024-05-08 21:37:26 +0300

    add a parametrized test

--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -1,19 +1,28 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.javaqamvn.rest.services.Rest;
 
 public class RestTest {
-    @Test
-    public void shouldCalcMonth() {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "scr/test/resources/Rest.csv")
+    public void shouldCalcMonth(int income, int expenses, int threshold, int expected) {
         Rest service = new Rest();
-
-        int income = 100000;
-        int expenses = 60000;
-        int threshold = 150000;
-        int expected = 2;
-
         int actual = service.calculate(income, expenses, threshold);
-
         Assertions.assertEquals(expected, actual);
     }
+    //   @Test
+    //  public void shouldCalcMonth() {
+    //     Rest service = new Rest();
+
+    //     int income = 100000;
+    //    int expenses = 60000;
+    //    int threshold = 150000;
+    //    int expected = 2;
+
+    //   int actual = service.calculate(income, expenses, threshold);
+
+    //   Assertions.assertEquals(expected, actual);
+    // }
 }
